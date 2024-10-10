79525546da1eb6c6596e7b4e4d595130892f12dd
Jason Jones <JsunJ.Jones@gmail.com>
2024-10-09 21:12:33 -0400

    build: initial docker container scaffolding

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+  },
+  // proxy setup (only for full stack apps)
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://backend:5001",
+        changeOrigin: true,
+      },
+    },
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173
+  },
+});
