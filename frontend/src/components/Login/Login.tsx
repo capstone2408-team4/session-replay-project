11db11bfe8ca509f48572ac11eedcd7365170e8a
Matt Register <rmregister0510@gmail.com>
2024-10-24 14:20:48 -0400

    feat: updated login component
    Co-Authored-By: Jason Jones <jsunj.jones@gmail.com>
    Co-Authored-By: Greg <97251635+gt-chase@users.noreply.github.com>
    Co-Authored-By: chadgratts <89122325+chadgratts@users.noreply.github.com>

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -2,27 +2,21 @@ import styles from './Login.module.css'
 import padlock from '../../assets/padlock.png'
 
 interface LoginProps {
-  showModal: boolean
-  onToggleModal: (e: any) => void
+  onToggleLoginModal: (e: any) => void
+  onToggleRegisterModal: (e: any) => void
 }
 
-function Login( { onToggleModal }: LoginProps) {
+function Login( { onToggleLoginModal, onToggleRegisterModal }: LoginProps) {
   return (
     <main className={styles.loginContainer}>
       <div className={styles.loginDialogBox}>
         <svg className={styles.padlock} width="160" height="240">
-          <image
-            href={padlock}
-            x='0'
-            y='0'
-            height='240'
-            width='160'
-          >
-          </image>
+          <image href={padlock} x='0' y='0' height='240' width='160' />
         </svg>
         <div className={styles.notLoggedIn}>
           <h1>Not Logged In</h1>
-          <p>Please <a onClick={onToggleModal}>log in</a> to access your project.</p>
+          <p>Please <a onClick={onToggleLoginModal}>log in</a> to access your project.</p>
+          <p className={styles.register}>Or <a onClick={onToggleRegisterModal}>register</a> your project.</p>
         </div>
       </div>
     </main>
