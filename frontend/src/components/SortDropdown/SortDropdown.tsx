2c3f6da0b3e0945a4d97e32152f57449219b4c45
Matt Register <rmregister0510@gmail.com>
2024-10-24 14:25:34 -0400

    feat: updated sort dropdown
    Co-Authored-By: Jason Jones <jsunj.jones@gmail.com>
    Co-Authored-By: Greg <97251635+gt-chase@users.noreply.github.com>
    Co-Authored-By: chadgratts <89122325+chadgratts@users.noreply.github.com>

--- frontend/src/components/SortDropdown/SortDropdown.tsx
+++ frontend/src/components/SortDropdown/SortDropdown.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import styles from './SortDropdown.module.css'
 
-function SortDropdown() {
-  const [dropdownIsOpen, setShowSortDropdown] = React.useState(true)
+interface SortDropdownProp {
+  onClosingClick: (e: any) => void
+}
 
+const SortDropdown = React.forwardRef<HTMLDivElement, SortDropdownProp>(( {onClosingClick}, ref) => {
   React.useEffect(() => {
+    document.addEventListener('click', onClosingClick);
 
+    return (() => {
+      document.removeEventListener('click', onClosingClick)
+    })
   }, [])
 
-
   return (
-    <div id='sortDropdown' className={styles.dropdownContainer}>
+    <div ref={ref} className={styles.dropdownContainer}>
       <ul>
-        <li>Sort Ascending ↑</li>
-        <li>Sort Descending ↓</li>
+        <li>Date Ascending ↑</li>
+        <li>Data Descending ↓</li>
+        <li>Sentiment Ascending ↑</li>
+        <li>Sentiment Descending ↓</li>
       </ul>
     </div>
   );
-}
+})
 
 export default SortDropdown;
