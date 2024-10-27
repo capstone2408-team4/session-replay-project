540814d639e5210d993baf8fb9410771c41ac177
Matt Register <rmregister0510@gmail.com>
2024-10-27 11:41:16 -0400

    chore: removed commented code, fixed type in summarizeSessionChunk
    Co-Authored-By: Jason Jones <jsunj.jones@gmail.com>
    Co-Authored-By: Greg <97251635+gt-chase@users.noreply.github.com>
    Co-Authored-By: chadgratts <89122325+chadgratts@users.noreply.github.com>

--- backend/src/models/AIParent.ts
+++ backend/src/models/AIParent.ts
@@ -3,20 +3,6 @@ import * as AIConfig from '../utils/aiModelsConfig';
 abstract class AIParent {
   protected abstract maxPromptLength: number;
 
-  // Splits data into manageable chunks of data to feed into the AI model
-  // Each model will have a different max prompt length as seen on line 4.
-  // protected splitIntoChunks(data: string): string[] {
-  //   const chunks: string[] = [];
-  //   let startIndex = 0;
-
-  //   while (startIndex < data.length) {
-  //     chunks.push(data.slice(startIndex, startIndex + this.maxPromptLength));
-  //     startIndex += this.maxPromptLength;
-  //   }
-
-  //   return chunks;
-  // }
-
   protected splitIntoChunks(data: string): string[] {
     const chunks: string[] = [];
     let startIndex = 0;
@@ -73,7 +59,13 @@ abstract class AIParent {
     const promises = chunks.map(chunk => this.query(AIConfig.SessionChunkPrompt, chunk));
     const summaries = await Promise.allSettled(promises);
 
-    return summaries.map(summary => summary.value);
+    return summaries.map(summary => {
+      if (summary.status === 'fulfilled') {
+        return summary.value
+      } else {
+        return '';
+      }
+    });
   } 
 }
 
